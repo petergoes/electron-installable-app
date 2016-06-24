79371ffb68cac60eb071ba2d615448719f12290c
Peter Goes <petergoes@gmail.com>
2016-06-24 13:12:14 +0200

    refactor(package): use different packaging approach

--- createinstaller.js
+++
@@ -1,7 +0,0 @@
-var electronInstaller = require('electron-winstaller');
-
-resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: '.',
-  });
-
-resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
